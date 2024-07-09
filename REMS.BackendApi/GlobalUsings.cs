52ab39952d7a6bc56bf6ad52e5a738556317e979
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-09 23:38:05 +0630

    [Complete] Global Usage Code Changes

---
+++ REMS.BackendApi/GlobalUsings.cs
@@ -0,0 +1,14 @@
+﻿// Global using directives
+
+global using Azure;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using REMS.BackendApi;
+global using REMS.Database.AppDbContextModels;
+global using REMS.Models;
+global using REMS.Models.Agent;
+global using REMS.Models.Review;
+global using REMS.Modules.Features.Agent;
+global using REMS.Modules.Features.Property;
+global using REMS.Modules.Features.Review;
