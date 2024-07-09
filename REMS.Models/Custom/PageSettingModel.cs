d5ee2d23e3a570346144b3d2c1587d72ccfa13c5
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-09 23:38:29 +0630

    [Complete] PageSetting Code Changes

--- REMS.Models/Custom/PageSettingModel.cs
+++ REMS.Models/Custom/PageSettingModel.cs
@@ -4,23 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace REMS.Models.Custom
+namespace REMS.Models.Custom;
+
+public class PageSettingModel
 {
-    public class PageSettingModel
+    public int TotalCount { get; set; }
+    public int PageCount { get; set; }
+    public int PageNo { get; set; }
+    public int PageSize { get; set; }
+    public bool IsEndOfPage => PageNo >= PageCount;
+
+    public PageSettingModel(int pageNo, int pageSize, int pageCount, int totalCount)
     {
-        public int TotalCount { get; set; }
-        public int PageCount { get; set; }
-        public int PageNo { get; set; }
-        public int PageSize { get; set; }
-        public bool IsEndOfPage => PageNo >= PageCount;
-
-        public PageSettingModel(int pageNo, int pageSize, int pageCount, int totalCount)
-        {
-            PageNo = pageNo;
-            PageSize = pageSize;
-            PageCount = pageCount;
-            TotalCount = totalCount;
-        }
+        PageNo = pageNo;
+        PageSize = pageSize;
+        PageCount = pageCount;
+        TotalCount = totalCount;
     }
-
 }
