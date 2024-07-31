f7d15aa67420739618528c2ce160a6595c68f36d
LinThitHtwe <linthit2745@gmail.com>
2024-08-01 00:39:27 +0630

    [restore] deleted codes for property

--- REMS.Database/AppDbContextModels/AppDbContext.cs
+++ REMS.Database/AppDbContextModels/AppDbContext.cs
@@ -31,10 +31,6 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-TTIU8JF8;Database=REMS;User ID=sa; Password=Minkhantthu3367;Integrated Security=True;Trusted_Connection=true;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Agent>(entity =>
