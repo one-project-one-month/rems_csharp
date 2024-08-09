a2eb3281d18bd71ede6fcecd551226838e432b7f
Hein Htet <ayathar123@gmail.com>
2024-08-10 01:39:24 +0630

    update table script for REMS

--- REMS.sql
+++ REMS.sql
@@ -1,6 +1,5 @@
 USE [REMS]
 GO
-/****** Object:  Table [dbo].[Agents]    Script Date: 7/8/2024 8:51:44 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,23 +9,20 @@ CREATE TABLE [dbo].[Agents](
 	[user_id] [int] NULL,
 	[agency_name] [nvarchar](100) NOT NULL,
 	[license_number] [nvarchar](50) NOT NULL,
-	[phone] [nvarchar](15) NULL,
-	[email] [nvarchar](100) NULL,
 	[address] [nvarchar](200) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[agent_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Appointments]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Appointments]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Appointments](
 	[appointment_id] [int] IDENTITY(1,1) NOT NULL,
-	[agent_id] [int] NULL,
 	[client_id] [int] NULL,
 	[property_id] [int] NULL,
 	[appointment_date] [date] NOT NULL,
@@ -36,10 +32,10 @@ CREATE TABLE [dbo].[Appointments](
 PRIMARY KEY CLUSTERED 
 (
 	[appointment_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Clients]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Clients]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -47,44 +43,42 @@ GO
 CREATE TABLE [dbo].[Clients](
 	[client_id] [int] IDENTITY(1,1) NOT NULL,
 	[user_id] [int] NULL,
-	[agent_id] [int] NULL,
 	[first_name] [nvarchar](100) NOT NULL,
 	[last_name] [nvarchar](100) NOT NULL,
-	[phone] [nvarchar](15) NULL,
-	[email] [nvarchar](100) NULL,
 	[address] [nvarchar](200) NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[client_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Listings]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Messages]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE [dbo].[Listings](
-	[listing_id] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[Messages](
+	[message_id] [int] IDENTITY(1,1) NOT NULL,
+	[sender_id] [int] NULL,
+	[receiver_id] [int] NULL,
 	[property_id] [int] NULL,
-	[agent_id] [int] NULL,
-	[date_listed] [datetime] NULL,
-	[listing_price] [decimal](18, 2) NOT NULL,
-	[status] [nvarchar](50) NOT NULL,
-	[description] [nvarchar](max) NULL,
+	[message_content] [nvarchar](max) NOT NULL,
+	[date_sent] [datetime] NULL,
+	[status] [nvarchar](50) NULL,
 PRIMARY KEY CLUSTERED 
 (
-	[listing_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	[message_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Properties]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Properties]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Properties](
 	[property_id] [int] IDENTITY(1,1) NOT NULL,
+	[agent_id] [int] NULL,
 	[address] [nvarchar](200) NOT NULL,
 	[city] [nvarchar](100) NOT NULL,
 	[state] [nvarchar](50) NOT NULL,
@@ -97,14 +91,18 @@ CREATE TABLE [dbo].[Properties](
 	[year_built] [int] NULL,
 	[description] [nvarchar](max) NULL,
 	[status] [nvarchar](50) NOT NULL,
-	[date_listed] [datetime] NULL,
+	[availiablity_type] [nvarchar](50) NOT NULL,
+	[minrental_period] [int] NULL,
+	[approvedby] [nvarchar](50) NULL,
+	[adddate] [datetime] NULL,
+	[editdate] [datetime] NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[property_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[PropertyImages]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[PropertyImages]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -118,10 +116,10 @@ CREATE TABLE [dbo].[PropertyImages](
 PRIMARY KEY CLUSTERED 
 (
 	[image_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Reviews]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Reviews]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -136,10 +134,10 @@ CREATE TABLE [dbo].[Reviews](
 PRIMARY KEY CLUSTERED 
 (
 	[review_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Transactions]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Transactions]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -147,20 +145,19 @@ GO
 CREATE TABLE [dbo].[Transactions](
 	[transaction_id] [int] IDENTITY(1,1) NOT NULL,
 	[property_id] [int] NULL,
-	[buyer_id] [int] NULL,
-	[seller_id] [int] NULL,
-	[agent_id] [int] NULL,
+	[client_id] [int] NULL,
 	[transaction_date] [datetime] NULL,
+	[rental_period] [int] NULL,
 	[sale_price] [decimal](18, 2) NOT NULL,
 	[commission] [decimal](18, 2) NULL,
 	[status] [nvarchar](50) NOT NULL,
 PRIMARY KEY CLUSTERED 
 (
 	[transaction_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-/****** Object:  Table [dbo].[Users]    Script Date: 7/8/2024 8:51:44 PM ******/
+/****** Object:  Table [dbo].[Users]    Script Date: 8/10/2024 1:31:22 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -176,20 +173,18 @@ CREATE TABLE [dbo].[Users](
 PRIMARY KEY CLUSTERED 
 (
 	[user_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-SET ANSI_PADDING ON
-GO
-/****** Object:  Index [UQ__Users__AB6E6164A21008DA]    Script Date: 7/8/2024 8:51:44 PM ******/
-ALTER TABLE [dbo].[Users] ADD UNIQUE NONCLUSTERED 
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
 (
 	[email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-ALTER TABLE [dbo].[Listings] ADD  DEFAULT (getdate()) FOR [date_listed]
+ALTER TABLE [dbo].[Messages] ADD  DEFAULT (getdate()) FOR [date_sent]
 GO
-ALTER TABLE [dbo].[Properties] ADD  DEFAULT (getdate()) FOR [date_listed]
+ALTER TABLE [dbo].[Properties] ADD  DEFAULT (getdate()) FOR [adddate]
+GO
+ALTER TABLE [dbo].[Properties] ADD  DEFAULT (getdate()) FOR [editdate]
 GO
 ALTER TABLE [dbo].[PropertyImages] ADD  DEFAULT (getdate()) FOR [date_uploaded]
 GO
@@ -202,26 +197,26 @@ GO
 ALTER TABLE [dbo].[Agents]  WITH CHECK ADD FOREIGN KEY([user_id])
 REFERENCES [dbo].[Users] ([user_id])
 GO
-ALTER TABLE [dbo].[Appointments]  WITH CHECK ADD FOREIGN KEY([agent_id])
-REFERENCES [dbo].[Agents] ([agent_id])
-GO
 ALTER TABLE [dbo].[Appointments]  WITH CHECK ADD FOREIGN KEY([client_id])
 REFERENCES [dbo].[Clients] ([client_id])
 GO
 ALTER TABLE [dbo].[Appointments]  WITH CHECK ADD FOREIGN KEY([property_id])
 REFERENCES [dbo].[Properties] ([property_id])
 GO
-ALTER TABLE [dbo].[Clients]  WITH CHECK ADD FOREIGN KEY([agent_id])
-REFERENCES [dbo].[Agents] ([agent_id])
-GO
 ALTER TABLE [dbo].[Clients]  WITH CHECK ADD FOREIGN KEY([user_id])
 REFERENCES [dbo].[Users] ([user_id])
 GO
-ALTER TABLE [dbo].[Listings]  WITH CHECK ADD FOREIGN KEY([agent_id])
-REFERENCES [dbo].[Agents] ([agent_id])
-GO
-ALTER TABLE [dbo].[Listings]  WITH CHECK ADD FOREIGN KEY([property_id])
+ALTER TABLE [dbo].[Messages]  WITH CHECK ADD FOREIGN KEY([property_id])
 REFERENCES [dbo].[Properties] ([property_id])
+GO
+ALTER TABLE [dbo].[Messages]  WITH CHECK ADD FOREIGN KEY([receiver_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Messages]  WITH CHECK ADD FOREIGN KEY([sender_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Properties]  WITH CHECK ADD FOREIGN KEY([agent_id])
+REFERENCES [dbo].[Agents] ([agent_id])
 GO
 ALTER TABLE [dbo].[PropertyImages]  WITH CHECK ADD FOREIGN KEY([property_id])
 REFERENCES [dbo].[Properties] ([property_id])
@@ -232,17 +227,11 @@ GO
 ALTER TABLE [dbo].[Reviews]  WITH CHECK ADD FOREIGN KEY([user_id])
 REFERENCES [dbo].[Users] ([user_id])
 GO
-ALTER TABLE [dbo].[Transactions]  WITH CHECK ADD FOREIGN KEY([agent_id])
-REFERENCES [dbo].[Agents] ([agent_id])
-GO
-ALTER TABLE [dbo].[Transactions]  WITH CHECK ADD FOREIGN KEY([buyer_id])
+ALTER TABLE [dbo].[Transactions]  WITH CHECK ADD FOREIGN KEY([client_id])
 REFERENCES [dbo].[Clients] ([client_id])
 GO
 ALTER TABLE [dbo].[Transactions]  WITH CHECK ADD FOREIGN KEY([property_id])
 REFERENCES [dbo].[Properties] ([property_id])
-GO
-ALTER TABLE [dbo].[Transactions]  WITH CHECK ADD FOREIGN KEY([seller_id])
-REFERENCES [dbo].[Clients] ([client_id])
 GO
 ALTER TABLE [dbo].[Reviews]  WITH CHECK ADD CHECK  (([rating]>=(1) AND [rating]<=(5)))
 GO
