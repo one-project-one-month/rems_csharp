7fbb691edb5a9fef85adc90a5e2e2ed56c1a45f0
ZinLinnhtet <mackk5504@gmail.com>
2024-08-28 16:55:58 +0630

    docs: updated db script

---
+++ REMS.Database/AppDbContextModels/Session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REMS.Database.AppDbContextModels;
+
+public partial class Session
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string RefreshToken { get; set; } = null!;
+
+    public DateTime ExpiredTime { get; set; }
+
+    public DateTime LastActiveTime { get; set; }
+}
