9c16df08f7c89fb0a8a1c0c20f97141b9f387ff8
Lint Action <lint-action@samuelmeuli.com>
2024-09-08 04:15:16 +0000

    Fix code style issues with dotnet_format

--- REMS.BackendApi/Features/Agent/AgentController.cs
+++ REMS.BackendApi/Features/Agent/AgentController.cs
@@ -89,10 +89,10 @@ public class AgentController : ControllerBase
         return Ok(agentList);
     }
 
-    [HttpGet("{pageNumber}/{pageSize}",Name = "AgentAll")]
+    [HttpGet("{pageNumber}/{pageSize}", Name = "AgentAll")]
     public async Task<IActionResult> AgentAll(int pageNumber, int pageSize)
     {
-        Result<AgentListResponseModel> agentList = await _blAgent.AgentAll(pageNumber,pageSize);
+        Result<AgentListResponseModel> agentList = await _blAgent.AgentAll(pageNumber, pageSize);
 
         return Ok(agentList);
     }
